8c60347bb4d3899b4e9626512e8c3f9b0e3d0551
DhanashreeKale <80916999+DhanashreeKale@users.noreply.github.com>
2022-01-07 20:38:23 +0530

    test.sql

---
+++ my-sql/test.sql
@@ -0,0 +1,13 @@
+CREATE TABLE 'studentData'
+(
+  'Name' varchar(40),
+  'Roll No' varchar(10),
+  'Address' varchar(100),
+  'Mobile Number' varchar(10),
+  'Pan Number' varchar(10),
+   PRIMARY KEY('Roll no')
+);
+
+INSERT INTO 'studentData' Values('Dhanashree Kale','MS2012','Pune,MAH','1234567891','3E3C34X43A');
+
+INSERT INTO 'studentData' Values('Ravi Kumar','MS2011','Mumbai,MAH','7834567891','YB3C34X43A');
